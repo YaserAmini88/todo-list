488fae57035ffb1af807f5cc843c7dc7a7566e64
elegent-codes <yaser.amini67@gmail.com>
2021-09-18 11:43:18 +0430

    Final Project

--- base/views.py
+++ base/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.views.generic.edit import CreateView,UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -18,6 +21,25 @@ class CustomLoginView(LoginView):
     def get_success_url(self):        # When user logged in, redirect to tasks page
         return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:       #after creation of user login and redirect it.
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):  #if user is logged in, it cannot see login and register page and will be redirected to tasks page.
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+
 class TaskList(LoginRequiredMixin, ListView):     # By default looks for task(model name)_list.html template
     model = Task                                  # LoginRequiredMixin - Restriction if user is not logged in
     context_object_name = 'tasks'
@@ -26,6 +48,13 @@ class TaskList(LoginRequiredMixin, ListView):     # By default looks for task(mo
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        #Search functionality
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            #context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView): # By default looks for task(model name)_detail.html template -
